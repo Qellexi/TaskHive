6c779c7feb47422c4667c3519877990883b35179
Qellexi <qellexi@gmail.com>
2025-10-21 14:37:38 +0200

    formatted within black command

--- TaskHive/wsgi.py
+++ TaskHive/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskHive.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TaskHive.settings")
 
 application = get_wsgi_application()
