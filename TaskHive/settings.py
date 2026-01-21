eac0a6be2fcf0cfd96599c14751cbcdcdd888e6a
Yuliia Vasylieva <yuliia.vasylieva@uni-jena.de>
2026-01-21 16:21:26 +0100

    Added settings

--- TaskHive/settings.py
+++ TaskHive/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-b#wm@is9wx%49(f5yeg*wg1oxhsl0w+$0u_j65w$q&!jjezcgt")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -152,5 +152,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+LOGIN_REDIRECT_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/"
 
 ASGI_APPLICATION = 'TaskHive.asgi.application'
--- TaskHive/urls.py
+++ TaskHive/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("management/", include("management.urls", namespace="management")),
+    path("", include("management.urls", namespace="management")),
     path("accounts/", include("django.contrib.auth.urls")),
 
 ]
